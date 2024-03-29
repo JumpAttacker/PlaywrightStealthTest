172c8159b4cec3d7bf4a0ea41f1a459cbded1c75
Vlad Matveev <vmatveev@ita-labs.ru>
2024-03-29 18:20:12 +0300

    Initial commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Initialize, install, use plugin and launch
+
+using PlaywrightExtraSharp;
+using PlaywrightExtraSharp.Models;
+using PlaywrightExtraSharp.Plugins.ExtraStealth;
+
+var playwrightExtra = await new PlaywrightExtra(BrowserTypeEnum.Chromium)
+    .Install()
+    .Use(new StealthExtraPlugin())
+    .LaunchAsync(new ()
+    {
+        Headless = false
+    });
+
+// Create a new page
+var context = await playwrightExtra.NewContextAsync();
+var page = await context.NewPageAsync();
+await page.GotoAsync("http://google.com");
